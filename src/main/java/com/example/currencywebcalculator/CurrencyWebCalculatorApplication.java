05532e88274c539e9c5af71053260c3a892e4171
Artyom Khotsin <a.khotsin@andersenlab.com>
2023-11-15 12:08:53 +0300

    Initial commit

---
+++ src/main/java/com/example/currencywebcalculator/CurrencyWebCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencywebcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyWebCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyWebCalculatorApplication.class, args);
+    }
+
+}
