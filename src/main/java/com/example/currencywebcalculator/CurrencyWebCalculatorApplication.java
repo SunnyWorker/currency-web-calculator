cdb48a01f220f768edd01bcf07203b7b1110214c
Artyom Khotsin <a.khotsin@andersenlab.com>
2023-11-15 12:18:35 +0300

    Adding postgres containerized DB Adding liquibase basis Refactor of init code

--- src/main/java/com/example/currencywebcalculator/CurrencyWebCalculatorApplication.java
+++ src/main/java/com/example/currencywebcalculator/CurrencyWebCalculatorApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurrencyWebCalculatorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CurrencyWebCalculatorApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(CurrencyWebCalculatorApplication.class, args);
+  }
 }
